c73802a522b60989a5f526c36660541ce8433fb8
kanit.ko <kanit.kos@gmail.com>
2017-01-19 15:10:06 +0700

    art

--- app/src/main/java/com/example/myapplication/SearchResultActivity.java
+++ app/src/main/java/com/example/myapplication/SearchResultActivity.java
@@ -1,13 +1,24 @@
 package com.example.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class SearchResultActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+//        String dateMeetingStr = getIntent().getStringExtra("date");
+//        String timeStartStr = getIntent().getStringExtra("timeStart");
+//        String timeEndStr = getIntent().getStringExtra("timeEnd");
+
+//        TextView textViewDateHeader = (TextView)findViewById(R.id.textViewDateHeader);
+//        textViewDateHeader.setText(dateMeetingStr);
+//
+//        TextView textViewTimeHeader = (TextView)findViewById(R.id.textViewTimeHeader);
+//        textViewTimeHeader.setText(timeStartStr +" - "+timeEndStr);
+
         setContentView(R.layout.activity_search_result);
     }
 }
