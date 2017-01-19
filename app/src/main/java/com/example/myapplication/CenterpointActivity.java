fab6852c54b44430bf2252683f9811fc6670deeb
kanit.ko <kanit.kos@gmail.com>
2017-01-19 16:23:07 +0700

    art

--- app/src/main/java/com/example/myapplication/CenterpointActivity.java
+++ app/src/main/java/com/example/myapplication/CenterpointActivity.java
@@ -6,12 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
-import com.example.myapplication.model.LoginRes;
 import com.example.myapplication.util.TokenUtil;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-
-import java.lang.reflect.Type;
 
 public class CenterpointActivity extends AppCompatActivity {
 
@@ -23,8 +18,6 @@ public class CenterpointActivity extends AppCompatActivity {
 
     public void reserveAction(View view){
         Intent intent = new Intent(this,ReserveActivity.class);
-        intent.putExtra("name3","xxx");
-        intent.putExtra("name4","yyy");
         startActivity(intent);
     }
 
