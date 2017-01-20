4d4d0a0fc29a7d075b40b90ddfc2b97b28eff608
Nu <nunthapong.s@kbtg.tech>
2017-01-20 16:53:33 +0700

    faofk

--- app/src/main/java/com/example/myapplication/CancleDetailActivity.java
+++ app/src/main/java/com/example/myapplication/CancleDetailActivity.java
@@ -3,11 +3,16 @@ package com.example.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.myapplication.model.Room;
+
 public class CancleDetailActivity extends AppCompatActivity {
 
+    Room roomInfo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cancle_detail);
+
+        roomInfo = (Room) getIntent().getSerializableExtra("roomInfo");
     }
 }
