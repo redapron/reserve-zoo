b24c20b9920911918c85ba28330c47ce62479d5d
kanit.ko <kanit.kos@gmail.com>
2017-01-20 11:39:15 +0700

    art

--- app/src/main/java/com/example/myapplication/model/Room.java
+++ app/src/main/java/com/example/myapplication/model/Room.java
@@ -6,20 +6,11 @@ package com.example.myapplication.model;
 
 public class Room {
 
-<<<<<<< HEAD
-    private String room;
-    private String sizez;
-    private int sizemax;
-    private boolean hasprojector;
-    private boolean hasvc;
-    private boolean haswb;
-=======
     private String Room;
     private int SizeMax;
     private boolean HasProjector;
     private boolean HasVC;
     private boolean HasWB;
->>>>>>> 5a20b2e06628c9c9ce618164ce79f5d67393560b
 
     public String getRoom() {
         return Room;
@@ -59,13 +50,5 @@ public class Room {
 
     public void setHasWB(boolean hasWB) {
         HasWB = hasWB;
-    }
-
-    public String getSizez() {
-        return sizez;
-    }
-
-    public void setSizez(String sizez) {
-        this.sizez = sizez;
     }
 }
