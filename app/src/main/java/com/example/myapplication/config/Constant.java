a89b3d58e7e4d2c95439c4f778cbd02b3413f8a6
kanit.ko <kanit.kos@gmail.com>
2017-01-25 12:45:18 +0700

    art

--- app/src/main/java/com/example/myapplication/config/Constant.java
+++ app/src/main/java/com/example/myapplication/config/Constant.java
@@ -6,7 +6,7 @@ package com.example.myapplication.config;
 
 public class Constant {
 
-    public static final String SERVER = "http://10.215.101.76:5000";
+    public static final String SERVER = "http://10.215.101.76:5000"; // test
     public static final String SERVICE_USER_LOGIN = SERVER+"/user/login";
     public static final String SERVICE_SLOT_VIEW = SERVER+"/slot/view";
     public static final String SERVICE_SLOT_MAKE = SERVER+"/slot/make";
